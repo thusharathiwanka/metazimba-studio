ad409e87b36f83964901b840cf2bd4ddb3c994fe
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-12 15:50:31 +0530

    CREATE: home page component added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import IntroSection from "../components/home/IntroSection";
+import Layout from "../layout/Layout";
+
+const Home = () => {
+	return (
+		<Layout>
+			<IntroSection />
+		</Layout>
+	);
+};
+
+export default Home;
