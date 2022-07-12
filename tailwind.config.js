649012eb597034ca72ec8969867a2162d4767ada
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-12 15:48:26 +0530

    CONFIG: tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"meta-grey": "#F3F1F6",
+				"meta-purple": "#6A55EA",
+			},
+		},
+	},
+	plugins: [],
+};
