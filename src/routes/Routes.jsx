23a7e0240edfddf67cb1a6e63a30e8b7df40979c
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-07-12 15:49:04 +0530

    CREATE: routes component created

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+const RoutesConfig = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
+	);
+};
+
+export default RoutesConfig;
